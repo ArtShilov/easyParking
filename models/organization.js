d00937ddd833e1ca3bdab933054e010e733c5b78
Artem <ArtShilov1991@yandex.ru>
2020-04-15 18:54:18 +0300

    asdas

--- models/organization.js
+++ models/organization.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const organizationSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    phone: { type: Number,  required: true },
+    phone: { type: Number,  required: true ,  unique: true },
     password: { type: String, required: true },
     parkingId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Parking' }],
 
