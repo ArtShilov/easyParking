b350bd1ebb14c304a0de5b5389d5054e03f858c5
Max_Gabrielov <maxgabrielov@gmail.com>
2020-04-17 11:04:55 +0300

    Release 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const useMiddleware = require("./middleware");
 const indexRouter = require("./routes/index");
 const organizationRouter = require('./routes/organization')
-
+const errorHandler = require('./middleware/error')
 const mapRouter = require("./routes/map");
 const useErrorHandlers = require("./middleware/error-handlers");
 
@@ -15,8 +15,9 @@ useMiddleware(app);
 // Подключаем импортированные маршруты с определенным url префиксом.
 app.use("/", indexRouter);
 app.use('/org', organizationRouter);
-
 app.use("/map", mapRouter);
+//страница 404
+app.use(errorHandler)
 
 useErrorHandlers(app);
 
