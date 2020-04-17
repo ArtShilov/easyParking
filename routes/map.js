ca94c4c4a84a0e564b48b207d1407c37c35446bb
Artem <ArtShilov1991@yandex.ru>
2020-04-17 15:57:28 +0300

    prelest

--- routes/map.js
+++ routes/map.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
     const { user } = req.session;
   if (req.session.user) {
 
-    res.render("map/index", { name: user.firstName, map: true, logout: "/logout"});
+    res.render("map/index", { name: user.firstName, map: true, logout: "/logout", logged: true});
   } else {
     res.redirect("/login");
   }
@@ -18,7 +18,8 @@ router.get('/reserv/:id', async (req, res) => {
   const id = req.params.id
   const parking = await Parking.findById(id)
   res.render('map/parking', {
-    parking
+    parking,
+    logged:true
   })
 })
 
@@ -28,7 +29,7 @@ router.post('/reserv', async (req, res) => {
   parking.countNow -= 1;
   await parking.save();
   const reservedTime = req.body.time;
-  res.render('/map', { reservedTime })
+  res.render('/map', { reservedTime, logged: true })
 })
 
 module.exports = router;
